a5dd3b13919f9b43445360d2de36e800de3432ea
Melissa Cejas <melissacejas@live.nl>
2019-10-17 15:27:13 +0200

    Edited files to implement login

--- app/src/main/java/com/example/cahiapp/Controller/CAHIAPP.java
+++ app/src/main/java/com/example/cahiapp/Controller/CAHIAPP.java
@@ -2,5 +2,7 @@ package com.example.cahiapp.Controller;
 
 import android.app.Application;
 
+
 public class CAHIAPP extends Application {
+
 }
--- app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
+++ app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cahiapp.Controller;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,12 @@ import com.example.cahiapp.R;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailBox;
+
+    private EditText passwordBox;
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
