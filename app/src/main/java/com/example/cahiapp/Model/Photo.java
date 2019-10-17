d90fd05e18fa1031a0fd553884f1876cf8e39547
Dario <dyedario@gmail.com>
2019-10-17 16:41:13 +0200

    Choose photo from Gallery code
    Added the choose photo function

--- app/src/main/java/com/example/cahiapp/Model/Photo.java
+++ app/src/main/java/com/example/cahiapp/Model/Photo.java
@@ -1,17 +1,26 @@
 package com.example.cahiapp.Model;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cahiapp.R;
 
 public class Photo extends AppCompatActivity {
 
-    private ImageView mimageView;
+    private ImageView mImageView;
+    private Button mChooseBtn;
     private static final int REAQUEST_IMAGE_CAPTURE = 101;
     private String photoName;
     private Enumerations photoType;
@@ -62,6 +71,9 @@ public class Photo extends AppCompatActivity {
         // TODO: 8-10-2019 fill in the function
     }
 
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
     public void takePicture(View view)
     {
         Intent imageTakeIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
@@ -78,7 +90,77 @@ public class Photo extends AppCompatActivity {
         if (requestCode == REAQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mimageView.setImageBitmap(imageBitmap);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_activity_main);
+
+        //Display Photo
+        mImageView = findViewById(R.id.imageView);
+        mChooseBtn = findViewById(R.id.choose_image_btn);
+
+        //Handle button action
+        mChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Check runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED) {
+                        //permission not granted, request it.
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup for permission runtime
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        //permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+                else {
+                //Android is lower than Android Pie
+                pickImageFromGallery();
+                }
+
+
+            }
+        });
+
+    }
+
+
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+
+    }
+
+    //handle result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length >0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+
+                }
+                else {
+                    //permission was denied
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
