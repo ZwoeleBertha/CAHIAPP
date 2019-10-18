23d0e0abc6cbb36c7c83c72fe3372c6be78fcd2a
Melissa Cejas <melissacejas@live.nl>
2019-10-18 15:05:36 +0200

    Added build.gradle dependencies to cloud store
    Tried to begin with home pages

---
+++ app/src/main/java/com/example/cahiapp/Controller/Home_Fragment.java
@@ -0,0 +1,38 @@
+package com.example.cahiapp.Controller;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cahiapp.R;
+
+public class Home_Fragment extends Fragment {
+
+    private HomeViewModel mViewModel;
+
+    public static Home_Fragment newInstance() {
+        return new Home_Fragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home__fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
