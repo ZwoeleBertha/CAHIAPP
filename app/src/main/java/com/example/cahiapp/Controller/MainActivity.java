2686548220ff76adf620134625fe80007fdf561b
shaquille s <shaquille.s@live.nl>
2019-10-31 19:21:26 +0100

    Added content to the landing page
    heb de lege landingpage(home) gevuld

--- app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
+++ app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.cahiapp.Controller;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.EventLog;
 import android.util.Log;
+import android.widget.ImageView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +18,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
+import com.bumptech.glide.Glide;
 import com.example.cahiapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,6 +30,10 @@ import com.google.firebase.database.core.view.Event;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private Context context = MainActivity.this;
 
     private static int SPLASH_TIME_OUT = 4000;
     private FirebaseAuth mAuth;
@@ -67,6 +73,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+
+        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView1);
+        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView2);
+        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView3);
+    }
+
+    private void loadImageByInternetURL(String URL, ImageView imageView){
+        Glide
+                .with(context)
+                .load(URL)
+                .into(imageView);
     }
 
 
