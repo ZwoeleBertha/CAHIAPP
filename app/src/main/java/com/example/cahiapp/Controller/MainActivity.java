852c7dd78ef00421e6a8fcc4b199dfef305e28b3
shaquille s <shaquille.s@live.nl>
2019-11-01 18:21:47 +0100

    front-end main activity is done
    de front end van main activity is klaar.

--- app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
+++ app/src/main/java/com/example/cahiapp/Controller/MainActivity.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import com.bumptech.glide.Glide;
+import com.example.cahiapp.Model.User;
 import com.example.cahiapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView1;
     private ImageView imageView2;
     private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
+    private ImageView imageView6;
+    private TextView sotd;
     private Context context = MainActivity.this;
+    private String URL = "https://www.oprolletjes.nl/wp-content/uploads/2018/03/Leger-gaat-het-niet-worden.png";
 
     private static int SPLASH_TIME_OUT = 4000;
     private FirebaseAuth mAuth;
@@ -43,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        sotd = (TextView)findViewById(R.id.sotd);
+        sotd.setText("It is my mother in laws birthday and this is her present!");
 
 
         //SignOut button functionaliteit
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent a = new Intent (MainActivity.this, MainActivity.class);
                     startActivity(a);
                 } else if (id == R.id.addPhoto) {
-                    Intent p = new Intent(MainActivity.this, AddPhotoActivity.class);
+                    Intent p = new Intent (MainActivity.this, AddPhotoActivity.class);
                     startActivity(p);
                 } else if (id == R.id.account) {
                     Intent i = new Intent (MainActivity.this, UserProfileActivity.class);
@@ -72,13 +79,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        imageView1 = (ImageView) findViewById(R.id.imageView1);
-        imageView2 = (ImageView) findViewById(R.id.imageView2);
-        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
 
-        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView1);
-        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView2);
-        loadImageByInternetURL("https://www.westernillinoisrealty.com/img/noimage.png",imageView3);
+        loadImageByInternetURL("https://firebasestorage.googleapis.com/v0/b/cahiapp.appspot.com/o/broom.jpg?alt=media&token=db5af279-35ec-4204-8fb2-2ff26af5cb0f",imageView1);
+        loadImageByInternetURL("https://firebasestorage.googleapis.com/v0/b/cahiapp.appspot.com/o/urn.jpg?alt=media&token=e3566560-b62f-4b4e-bb8a-d35c96196d49",imageView2);
+        loadImageByInternetURL(URL,imageView3);
+        loadImageByInternetURL(URL,imageView4);
+        loadImageByInternetURL(URL,imageView5);
+        loadImageByInternetURL(URL,imageView6);
+
     }
 
     private void loadImageByInternetURL(String URL, ImageView imageView){
